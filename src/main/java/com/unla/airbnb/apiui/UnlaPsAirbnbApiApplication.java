15be53cbcf91b05c9cfe8782c57a49e9c5f836de
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2025-05-13 20:00:26 -0300

    feat: initial commit

---
+++ src/main/java/com/unla/airbnb/apiui/UnlaPsAirbnbApiApplication.java
@@ -0,0 +1,13 @@
+package com.unla.airbnb.apiui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlaPsAirbnbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlaPsAirbnbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/airbnb/apiui/UnlaPsAirbnbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.airbnb.apiui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlaPsAirbnbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
